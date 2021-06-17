16c686008733a2e6ab55084a6bad3a7de66484f7
Ahmed Ossama <58595131+AhmedOssama22dev@users.noreply.github.com>
2021-06-17 15:21:58 +0200

    Revert "Delete cmake-build-debug directory"
    This reverts commit ca91aa49ddb882d3eae3b792d8b6829b211a363f.

---
+++ cmake-build-debug/CMakeFiles/RedBlackDictionary.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Kimo Store/Documents/GitHub/RedBlackDictionary/RedBlackNode.cpp" "C:/Users/Kimo Store/Documents/GitHub/RedBlackDictionary/cmake-build-debug/CMakeFiles/RedBlackDictionary.dir/RedBlackNode.cpp.obj"
+  "C:/Users/Kimo Store/Documents/GitHub/RedBlackDictionary/RedBlackTree.cpp" "C:/Users/Kimo Store/Documents/GitHub/RedBlackDictionary/cmake-build-debug/CMakeFiles/RedBlackDictionary.dir/RedBlackTree.cpp.obj"
+  "C:/Users/Kimo Store/Documents/GitHub/RedBlackDictionary/main.cpp" "C:/Users/Kimo Store/Documents/GitHub/RedBlackDictionary/cmake-build-debug/CMakeFiles/RedBlackDictionary.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/RedBlackDictionary.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RedBlackDictionary.dir/RedBlackNode.cpp.obj"
+  "CMakeFiles/RedBlackDictionary.dir/RedBlackTree.cpp.obj"
+  "CMakeFiles/RedBlackDictionary.dir/main.cpp.obj"
+  "RedBlackDictionary.exe"
+  "RedBlackDictionary.exe.manifest"
+  "RedBlackDictionary.pdb"
+  "libRedBlackDictionary.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RedBlackDictionary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
