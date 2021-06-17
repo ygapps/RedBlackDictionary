6460ff78702fa526650faadb5d863a9544bddc8d
Ahmed Ossama <58595131+AhmedOssama22dev@users.noreply.github.com>
2021-06-17 15:15:41 +0200

    deleted unnecessary files

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/6th term/RBT
+# Install script for directory: C:/Users/Kimo Store/Documents/GitHub/RedBlackDictionary
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/6th term/RBT/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Kimo Store/Documents/GitHub/RedBlackDictionary/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
